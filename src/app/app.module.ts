dd245982dead298fdf8d966dcd836015414aa7fe
isaac r <ierubiot@gmail.com>
2019-10-30 17:06:54 -0400

    58 pipes starting changes!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// Routes
+import { APP_ROUTING } from './app.routes';
+
+// Services
+import { HeroesService } from './services/heroes.service';
+
+
+// Components
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/shared/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    HeroesComponent,
+    AboutComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
